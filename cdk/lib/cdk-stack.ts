c586bd263dd15395ff409479bb296b544c0a949f
Nazareh <nazarehturmina@gmail.com>
2023-05-18 13:54:08 +0000

    Config SES on CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,20 +1,11 @@
-import * as cdk from "aws-cdk-lib";
-import {
-  JsonSchemaType,
-  LambdaIntegration,
-  Model,
-  RestApi,
-} from "aws-cdk-lib/aws-apigateway";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-
-import {
-  Runtime,
-} from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
-
+import * as cdk from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import { RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
-
+import * as ses from 'aws-cdk-lib/aws-ses';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -22,40 +13,40 @@ export class CdkStack extends cdk.Stack {
 
     const matchFunction = new NodejsFunction(this, "Match", {
       entry: "lambda/match-fn.ts",
-      runtime: Runtime.NODEJS_18_X
+      runtime: lambda.Runtime.NODEJS_18_X
     });
 
-    const api = new RestApi(this, "reabold-api", {
-      restApiName: "Widget Service",
-      description: "This service serves widgets.",
+    const api = new apigateway.RestApi(this, "reabold-api", {
+      restApiName: "Reabold Api",
+      description: "This API receives Padel match results",
     });
 
-    const matchModel = new Model(this, "model-validator", {
+    const matchModel = new apigateway.Model(this, "model-validator", {
       restApi: api,
       contentType: "application/json",
       description: "To validate the request body",
       modelName: "matchmodelcdk",
       schema: {
-        type: JsonSchemaType.OBJECT,
-        required: ["datetime", "court", "wins", "losses"],
+        type: apigateway.JsonSchemaType.OBJECT,
+        required: ["datetime", "court", "wins", "losses","team1","team2"],
         properties: {
-          datetime: { type: JsonSchemaType.STRING },
-          wins: { type: JsonSchemaType.INTEGER },
-          losses: { type: JsonSchemaType.INTEGER },
+          datetime: { type: apigateway.JsonSchemaType.STRING },
+          wins: { type: apigateway.JsonSchemaType.INTEGER },
+          losses: { type: apigateway.JsonSchemaType.INTEGER },
           team1: {
-            type: JsonSchemaType.OBJECT,
+            type: apigateway.JsonSchemaType.OBJECT,
             required: ["player1", "player2"],
             properties: {
-              player1: { type: JsonSchemaType.STRING },
-              player2: { type: JsonSchemaType.STRING },
+              player1: { type: apigateway.JsonSchemaType.STRING },
+              player2: { type: apigateway.JsonSchemaType.STRING },
             },
           },
           team2: {
-            type: JsonSchemaType.OBJECT,
+            type: apigateway.JsonSchemaType.OBJECT,
             required: ["player1", "player2"],
             properties: {
-              player1: { type: JsonSchemaType.STRING },
-              player2: { type: JsonSchemaType.STRING },
+              player1: { type: apigateway.JsonSchemaType.STRING },
+              player2: { type: apigateway.JsonSchemaType.STRING },
             },
           },
         },
@@ -69,7 +60,7 @@ export class CdkStack extends cdk.Stack {
 
     api.root.addResource("matches").addMethod(
       "POST",
-      new LambdaIntegration(matchFunction, {}),
+      new apigateway.LambdaIntegration(matchFunction, {}),
       {
         requestModels: { 'application/json': matchModel },
         requestValidator: basicValidator,
@@ -79,9 +70,41 @@ export class CdkStack extends cdk.Stack {
     const matchTable = new dynamodb.Table(this, 'MatchTable', {
       tableName: 'match',
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      removalPolicy: RemovalPolicy.DESTROY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
     matchTable.grantReadWriteData(matchFunction)
+
+    const senderEmail = 'nazarehturmina@gmail.com';
+
+    const senderIdentity = new ses.CfnEmailIdentity(this, 'NazGmailIdentity', {
+      emailIdentity: senderEmail
+    });
+
+    const receiverIdentity = new ses.CfnEmailIdentity(this, 'NazYahooIdentity', {
+      emailIdentity: 'nazarehturmina@yahoo.com.br'
+    });
+
+    const sesPolicy = 
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: [
+            'ses:SendEmail',
+            'ses:SendRawEmail',
+            'ses:SendTemplatedEmail',
+          ],
+          resources: [
+            `arn:aws:ses:${process.env.CDK_DEPLOY_REGION}:${
+              cdk.Stack.of(this).account
+            }:identity/${senderEmail}`,
+          ],
+          conditions: {
+            'StringEquals': {
+              'ses:FromAddress': senderEmail,
+            },
+          },
+        })
+
+    matchFunction.addToRolePolicy(sesPolicy)
   }
 }
